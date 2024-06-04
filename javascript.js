d68cce4ab7a19d015cff61e6f5f8baf13ad30f54
kewlaid <145947170+kewla1d@users.noreply.github.com>
2024-06-04 02:18:29 -0700

    just a few changes lol

--- javascript.js
+++ javascript.js
@@ -1,11 +1,40 @@
 /* scroll move */
 window.addEventListener('scroll', function() {
-    var cover = document.querySelector('div#CoverPage');
+    var Home = document.querySelector('.docklink#Home');
+    var AbtMe = document.querySelector('.docklink#AbtMe');
+    var Social = document.querySelector('.docklink#Social');
+    var dockblur = document.querySelector('#dock_blur');
     var scrollTop = window.scrollY;
   
     if (scrollTop > 0) {
-      cover.classList.add('scrolled');
+      dockblur.classList.remove('reached');
     } else {
-      cover.classList.remove('scrolled');
+      dockblur.classList.add('reached');
     }
+
+    if (scrollTop > -100) {
+      Home.classList.add('reached');
+    } else {
+      Home.classList.remove('reached');
+    }
+
+    if (scrollTop >= 607) {
+      AbtMe.classList.add('reached');
+      Home.classList.remove('reached');
+    } else {
+      AbtMe.classList.remove('reached');
+    }
+
+    if (scrollTop >= 1214) {
+      Social.classList.add('reached');
+      AbtMe.classList.remove('reached');
+    } else {
+      Social.classList.remove('reached');
+    }
+}
+);
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  console.log(scroll)
 });
